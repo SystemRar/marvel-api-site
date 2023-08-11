0f782e7fb5d0a7232ea3e947215349b4528dc96c
Ian Covaliuc <yan.covaliuc@gmail.com>
2023-08-11 18:56:07 +0300

    Enable auto-switch random hero

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -12,10 +12,10 @@ const RandomChar = () => {
 
 	useEffect(() => {
 		updateChar();
-		//const timerId = setInterval(updateChar, 60000);
+		const timerId = setInterval(updateChar, 60000);
 
 		return () => {
-			//clearInterval(timerId)
+			clearInterval(timerId)
 		}
 		// eslint-disable-next-line
 	}, [])
